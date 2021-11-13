d12be12563e562cf2d3d9ddabcebae29199689ab
Stephen <champs4@hotmail.com>
2021-11-13 22:32:08 +0800

    fixed bugs about the device

--- src/predict.py
+++ src/predict.py
@@ -6,12 +6,12 @@ from src.net import Net
 
 def predict(img_numpy, ckpt_path):
     transform = transforms.Compose([transforms.ToTensor()])
-
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     img_torch = transform(img_numpy.astype(np.float32))
-    img_torch = img_torch.unsqueeze(dim=0)
+    img_torch = img_torch.unsqueeze(dim=0).to(device)
 
-    net = Net()
+    net = Net().to(device)
     net.eval()
-    net.load_state_dict(torch.load(ckpt_path))
+    net.load_state_dict(torch.load(ckpt_path, map_location=torch.device(device)))  
     log_preds = net(img_torch)
     return log_preds
