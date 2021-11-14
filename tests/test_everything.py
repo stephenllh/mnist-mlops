4961b8362861be246f43311a39f600f5d68411cf
Stephen <champs4@hotmail.com>
2021-11-14 21:14:26 +0800

    removed train and test loop test

--- tests/test_everything.py
+++ tests/test_everything.py
@@ -4,7 +4,6 @@ import requests
 import torch
 import numpy as np
 from src.net import Net
-from src.main import train, tst, get_loader
 from src.predict import predict
 
 
@@ -18,25 +17,6 @@ def test_net(net):
     x = torch.zeros(BATCH_SIZE, 1, 28, 28)
     out = net(x)
     assert out.shape == (BATCH_SIZE, 10)
-
-
-class Args:
-    dry_run = True
-    log_interval = 100
-
-
-def test_train_loop(net):
-    device = "cpu"
-    train_loader = get_loader(8, True)
-    optimizer = torch.optim.Adadelta(net.parameters(), lr=0.001)
-    epoch = 1
-    train(Args, net, device, train_loader, optimizer, epoch)
-
-
-def test_testing_loop(net):
-    device = "cpu"
-    test_loader = get_loader(8, False)
-    tst(net, device, test_loader)
 
 
 def test_predict():
